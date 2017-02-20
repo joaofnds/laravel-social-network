8d2ffdec33420745ebf6530ed3fe85cc4c925b69
João Fernandes <joaovitorfernandes2@gmail.com>
2017-02-20 08:14:11 -0300

    Complete lesson 3

---
+++ app/Profile.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    protected $fillable = ['location', 'about'];
+
+    public function profile()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
