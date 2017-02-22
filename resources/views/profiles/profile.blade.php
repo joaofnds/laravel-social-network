29efa4b84d63a3237ff4fb02f5bf52966be3d833
João Fernandes <joaovitorfernandes2@gmail.com>
2017-02-22 16:26:57 -0300

    Complete lesson 7

--- resources/views/profiles/profile.blade.php
+++ resources/views/profiles/profile.blade.php
@@ -3,16 +3,26 @@
 @section('content')
     <div class="container">
         <div class="col-lg-6 col-lg-offset-3">
+
             <div class="panel panel-default">
+
                 <div class="panel-heading">
                     {{ $user->name }}'s Profile
                 </div>
+
                 <div class="panel-body text-center">
                     <img class="profile-image" src="{{ Storage::url($user->avatar) }}" alt="{{ $user->name }}'s avatar">
-                </div>
-                <div class="text-center">
+
+                    <p class="text-center">{{ $user->profile->location }}</p>
+
                     <a href="{{ route('profile.edit', ['slug' => $user->slug]) }}" class="btn btn-default">Edit Your Profile</a>
                 </div>
+
+            </div>
+
+            <div class="panel panel-default">
+                <div class="panel-heading">About {{ $user->name }}</div>
+                <div class="panel-body text-center">{{ $user->profile->about }}</div>
             </div>
         </div>
     </div>
